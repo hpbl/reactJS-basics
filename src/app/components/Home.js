307aed63ca7323a90964a42a69a6007d9cec4bba
Hilton Pintor <hpbl@cin.ufpe.br>
2017-05-31 15:24:00 -0300

    Handled events
    without state update

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,13 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export class Home extends React.Component {
+  constructor (props) {
+    super()
+    this.age = props.age
+  }
+
+  onMakeMeOlder () {
+    this.age += 3
+    console.log(this.age)
+  }
+
   render () {
     return (
       <div>
         <p>In a new Component!</p>
 
         <p>Name: {this.props.name}</p>
-        <p>Age: {this.props.age}</p>
+        <p>Age: {this.age}</p>
 
         <p>User Object Name: {this.props.otherPerson.name}</p>
         <h4>Hobbies:</h4>
@@ -17,6 +27,8 @@ export class Home extends React.Component {
         </ul>
         <hr />
         {this.props.children}
+        <hr />
+        <button onClick={() => this.onMakeMeOlder()}>Make me Older!</button>
       </div>
     )
   }
